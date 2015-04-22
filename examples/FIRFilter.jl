bb5c5aa9ad7250f46f4aafb389c15feaabbcb079
Jay Kickliter <jay@kickliter.com>
2015-04-22 09:32:17 -0600

    Added hardware (speaker) sample limiting

--- examples/FIRFilter.jl
+++ examples/FIRFilter.jl
@@ -1,8 +1,7 @@
 using DSP
 
-taps = digitalfilter(Lowpass(0.3), WindowFIR(transitionwidth=0.05))
+taps = digitalfilter(Lowpass(0.01), WindowFIR(transitionwidth=0.1))
 
 const myfilt = FIRFilter(taps)
 
-
-work(y,x) = filt!(y,myfilt,x)
+work(y,x) = filt!(y,myfilt,x)add
